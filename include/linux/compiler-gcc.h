554d93ad4f0cad9ce64e4279ebb119405b899f08
Andi Kleen <ak@linux.intel.com>
2015-03-31 03:45:39 -0700

    lto: Add __noreorder and mark initcalls __noreorder
    gcc 5 has a new no_reorder attribute that prevents top level
    reordering only for that symbol.
    
    Kernels don't like any reordering of initcalls between files, as several
    initcalls depend on each other. LTO previously needed to use
    -fno-toplevel-reordering to prevent boot failures.
    
    Add a __noreorder wrapper for the no_reorder attribute and use
    it for initcalls.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- include/linux/compiler-gcc.h
+++ include/linux/compiler-gcc.h
@@ -151,6 +151,11 @@
 #define COMPILER_HAS_GENERIC_BUILTIN_OVERFLOW 1
 #endif
 
+#if __GNUC__ >= 5
+/* Avoid reordering a top level statement */
+#define __noreorder    __attribute__((no_reorder))
+#endif
+
 /*
  * Turn individual warnings and errors on and off locally, depending
  * on version.
