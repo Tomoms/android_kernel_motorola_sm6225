1ef0b954c256d69c9b87f3c1f2294b3bcc829495
Park Ju Hyung <qkrwngud825@gmail.com>
2019-05-16 22:02:35 +0900

    msm_bus: remove tracer
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Change-Id: Ic66a7d7f2d0829fb9a2f3d75c96ed434eb0071ba

--- drivers/soc/qcom/msm_bus/msm_bus_bimc_adhoc.c
+++ drivers/soc/qcom/msm_bus/msm_bus_bimc_adhoc.c
@@ -406,7 +406,6 @@ static void bimc_set_static_qos_bw(void __iomem *base, unsigned int qos_freq,
 	MSM_BUS_DBG("%s: BKE parameters: gp %d, gc %d, thm %d thl %d thh %d",
 			__func__, gp, gc, thm, thl, thh);
 
-	trace_bus_bke_params(gc, gp, thl, thm, thl);
 	set_qos_bw_regs(base, mport, thh, thm, thl, gp, gc);
 }
 
@@ -554,7 +553,6 @@ static int msm_bus_bimc_set_bw(struct msm_bus_node_device_type *dev,
 		}
 
 		qbw.bw = bw + info->qos_params.bw_buffer;
-		trace_bus_bimc_config_limiter(info->id, bw);
 
 		/* Default to gp of 5us */
 		qbw.gp = (info->qos_params.gp ?
