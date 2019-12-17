18b9739155adeb4385be3528ac35000a89e1544d
Eric Dumazet <edumazet@google.com>
2019-12-16 18:51:03 -0800

    net: annotate lockless accesses to sk->sk_pacing_shift
    [ Upstream commit 7c68fa2bddda6d942bd387c9ba5b4300737fd991 ]
    
    sk->sk_pacing_shift can be read and written without lock
    synchronization. This patch adds annotations to
    document this fact and avoid future syzbot complains.
    
    This might also avoid unexpected false sharing
    in sk_pacing_shift_update(), as the compiler
    could remove the conditional check and always
    write over sk->sk_pacing_shift :
    
    if (sk->sk_pacing_shift != val)
    	sk->sk_pacing_shift = val;
    
    Change-Id: Ie016b50c0963cebd6b28d543d52ea5a23e18749f
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/core/sock.c
+++ net/core/sock.c
@@ -2935,7 +2935,7 @@ void sock_init_data_uid(struct socket *sock, struct sock *sk, kuid_t uid)
 
 	sk->sk_max_pacing_rate = ~0UL;
 	sk->sk_pacing_rate = ~0UL;
-	sk->sk_pacing_shift = 10;
+	WRITE_ONCE(sk->sk_pacing_shift, 10);
 	sk->sk_incoming_cpu = -1;
 
 	sk_rx_queue_clear(sk);
