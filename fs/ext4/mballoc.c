6646fd76b0e0c87572f613fc1e2b54e4330e17b1
Sultan Alsawaf <sultan@kerneltoast.com>
2020-03-10 19:53:02 -0700

    ext4: Allocate 128-byte allocation context on the stack
    The allocation context structure is 128 bytes and thus easily fits on
    the stack. There's no need to allocate it dynamically.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Change-Id: I2fc426d7fe92cc4f4868a214870b1df13525f066

--- fs/ext4/mballoc.c
+++ fs/ext4/mballoc.c
@@ -4606,7 +4606,7 @@ repeat:
 			goto errout;
 		}
 	}
-	if (likely(ac->ac_status == AC_STATUS_FOUND)) {
+	if (likely(ac.ac_status == AC_STATUS_FOUND)) {
 		*errp = ext4_mb_mark_diskspace_used(&ac, handle);
 		if (*errp) {
 			ext4_discard_allocated_blocks(&ac);
