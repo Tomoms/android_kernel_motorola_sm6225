11b9dbabe3e1347f95ff1d9210b70ae5fa641301
Tommaso Fonda <fondatommaso2@gmail.com>
2023-11-02 10:39:44 +0100

    devfreq: Adapt devfreq_boost to bengal/khaje
    Change-Id: I8e203d86efc90fb8b547e5229db23b34b74e417b

--- kernel/fork.c
+++ kernel/fork.c
@@ -2383,7 +2383,7 @@ long _do_fork(struct kernel_clone_args *args)
 
 	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
 	if (task_is_zygote(current))
-		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
+		devfreq_boost_kick_max(DEVFREQ_CPU_CPU_DDR_BW, 50);
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
