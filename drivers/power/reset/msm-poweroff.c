dd2414774a204ee060b83b089430253e6d8c5b16
Tommaso Fonda <fondatommaso2@gmail.com>
2024-03-18 14:12:05 +0100

    Revert "power: reset: msm: Always perform a warm reboot"
    This reverts commit 47f78fe93464ffda6de65b4203ba518dfedf16b5.

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -75,7 +75,7 @@ static bool dload_mode_enabled;
 static void *emergency_dload_mode_addr;
 static bool scm_dload_supported;
 
-static bool force_warm_reboot = true;
+static bool force_warm_reboot;
 
 struct work_struct reboot_work;
 struct input_handler handler;
@@ -814,6 +814,9 @@ static int msm_restart_probe(struct platform_device *pdev)
 	set_dload_mode(download_mode);
 	if (!download_mode)
 		scm_disable_sdi();
+
+	force_warm_reboot = of_property_read_bool(dev->of_node,
+						"qcom,force-warm-reboot");
 
 	INIT_WORK(&reboot_work, reboot_work_func);
 	handler.event = input_event_handler;
